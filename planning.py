17381a683b6813746edef28634e05e96ae3de281
peach <pichayuth.c@gmail.com>
2016-04-28 12:15:04 +0700

    modify planning.py

---
+++ planning.py
@@ -0,0 +1,12 @@
+import calendar
+import time
+print ('plannig plant\n')
+irspringer = 24*24
+qlspringer = 14*8
+total_seed = irspringer+qlspringer
+print ('main seed = ',irspringer)
+print ('secondary seed = ',qlspringer)
+print ('total seed = ',total_seed)
+print ('\n')
+print ('today is ',time.strftime("%Y-%m-%d"))
+print (calendar.month(2016,5))
