7f57a5517b6b33186628d2cc6048f33a0c7bfb55
peach <pichayuth.c@gmail.com>
2016-04-28 13:33:51 +0700

    add grass

--- planning.py
+++ planning.py
@@ -4,9 +4,11 @@ print ('plannig plant\n')
 irspringer = 24*24
 qlspringer = 14*8
 total_seed = irspringer+qlspringer
+grass = 30*100
 print ('main seed = ',irspringer)
 print ('secondary seed = ',qlspringer)
 print ('total seed = ',total_seed)
+print ('grass = ',grass)
 print ('\n')
 print ('today is ',time.strftime("%Y-%m-%d"))
 print (calendar.month(2016,5))
