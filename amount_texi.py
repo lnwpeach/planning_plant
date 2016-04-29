ec82d0f174d204c00b4a71f976b0de11196a7cdf
peach <pichayuth.c@gmail.com>
2016-04-29 09:41:39 +0700

    add amount_text.py

---
+++ amount_texi.py
@@ -0,0 +1,28 @@
+ratio = open('ratio.txt','r')
+print ('\ttest texi program')
+print (ratio.read())
+amount = 0
+distance = float(input('Enter Distance : '))
+amount += 35
+if distance <= 1:
+    amount = amount 
+elif distance <= 5:
+    distance -= 1
+    amount += distance*2
+elif distance <= 10:
+    distance -= 5
+    amount += 8
+    amount += distance*2.5
+elif distance <= 20:
+    distance -= 10
+    amount = 55.5
+    amount += distance*3
+elif distance <= 50:
+    distance -= 20
+    amount = 85.5
+    amount += distance*3.5
+else:
+    distance -= 50
+    amount = 190.5 
+    amount += distance*5
+print ('amount = %.2f Bath'%(amount))
--- planning.py
+++ planning.py
@@ -5,8 +5,12 @@ irspringer = 24*24
 qlspringer = 14*8
 total_seed = irspringer+qlspringer
 grass = 30*100
+cranb_main_g = irspringer*240
+other_g = qlspringer*80
 print ('main seed = ',irspringer)
+print ('cranberry main G = ',cranb_main_g)
 print ('secondary seed = ',qlspringer)
+print ('other G = ',other_g)
 print ('total seed = ',total_seed)
 print ('grass = ',grass)
 print ('\n')
